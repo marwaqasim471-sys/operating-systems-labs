765c7991e183a7dc02ab21034eeb445006559913
marwaqasim471-sys <marwaqasim471@gmail.com>
2025-12-01 23:56:35 +0300

    Update main.c

--- example-01/main.c
+++ example-01/main.c
@@ -1,14 +1,13 @@
 // example-01: read() and write() system calls
-
 #include <unistd.h>
 
 int main(void) {
     char buffer[20];
 
-    // Read 10 bytes from standard input (keyboard)
+    // Read up to 10 bytes from standard input (keyboard)
     read(0, buffer, 10);
 
-    // Write 10 bytes to standard output (screen)
+    // Write the same 10 bytes to standard output (screen)
     write(1, buffer, 10);
 
     return 0;
